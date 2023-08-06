51239e1334c47eb6c0d30ded3ee3926c0819e186
Tengwei Cai <tengweicai@gmail.com>
2023-08-06 17:21:29 +0800

    init server part

---
+++ cpp/server/grpc/inference_server.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+
+#include <grpc/support/log.h>
+#include <grpcpp/grpcpp.h>
+
+int main(int argc, char** argv) {
+    return 0;
+}
