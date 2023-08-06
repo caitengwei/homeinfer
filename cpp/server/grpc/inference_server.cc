6c22983f4b78e7a12021125f92fdde436fe38116
Tengwei Cai <tengweicai@gmail.com>
2023-08-07 01:12:16 +0800

    Fix pb file generate

--- cpp/server/grpc/inference_server.cc
+++ cpp/server/grpc/inference_server.cc
@@ -6,6 +6,12 @@
 #include <grpc/support/log.h>
 #include <grpcpp/grpcpp.h>
 
+#include "generated/inference.grpc.pb.h"
+#include "generated/inference.pb.h"
+
+using namespace server::grpc;
+
 int main(int argc, char** argv) {
+    printf("Hello World!\n");
     return 0;
 }
